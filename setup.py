82aa952e366f39e26bf7c55f3596f8b82306a004
Jordan H <jordan@hrvy.uk>
2023-09-03 23:16:38 +0100

    boilerplate and gh actions

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Rooster Money setup"""
+
+from setuptools import setup
+
+setup(name="pyfamilysafety",
+      version="0.0.1",
+      description="Microsoft Family Safety API library",
+      url="https://github.com/pantherale0/pyfamilysafety",
+      author="pantherale0",
+      author_email="support@system32.uk",
+      license='MIT',
+      install_requires="aiohttp")
