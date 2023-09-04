2fec9a18de68625d7da273bd1eb7a52502d9d6a8
Jordan H <jordan@hrvy.uk>
2023-09-04 14:11:30 +0100

    0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="pyfamilysafety",
-      version="0.0.1",
+      version="0.0.2",
       description="Microsoft Family Safety API library",
       url="https://github.com/pantherale0/pyfamilysafety",
       author="pantherale0",
