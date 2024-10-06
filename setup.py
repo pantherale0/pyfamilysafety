b5961eff3e53ac6f7d704f621ac6b8c16c6ab6f4
Jordan <jordan@hrvy.uk>
2024-10-07 00:00:23 +0100

    modern build

--- setup.py
+++ setup.py
@@ -1,12 +1,62 @@
-"""Rooster Money setup"""
+import re
 
-from setuptools import setup
+import setuptools
 
-setup(name="pyfamilysafety",
-      version="0.3.0",
-      description="Microsoft Family Safety API library",
-      url="https://github.com/pantherale0/pyfamilysafety",
-      author="pantherale0",
-      author_email="support@system32.uk",
-      license='MIT',
-      install_requires="aiohttp")
+with open('README.md', 'r', encoding='utf8') as readme_file:
+    long_description = readme_file.read()
+
+# Inspiration: https://stackoverflow.com/a/7071358/6064135
+with open('pyfamilysafety/_version.py', 'r', encoding='utf8') as version_file:
+    version_groups = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
+    if version_groups:
+        version = version_groups.group(1)
+    else:
+        raise RuntimeError('Unable to find version string!')
+
+REQUIREMENTS = [
+    "aiohttp >= 3.7.0"
+]
+
+DEV_REQUIREMENTS = [
+    'bandit == 1.7.*',
+    'black == 23.*',
+    'build == 0.10.*',
+    'flake8 == 6.*',
+    'isort == 5.*',
+    'mypy == 1.5.*',
+    'pytest == 7.*',
+    'pytest-cov == 4.*',
+    'twine == 4.*',
+]
+
+setuptools.setup(
+    name='pyfamilysafety',
+    version=version,
+    description='Microsoft Family Safety API library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/pantherale0/pyfamilysafety',
+    author='pantherale0',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    package_data={
+        'pyfamilysafety': [
+            'py.typed',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIREMENTS,
+    extras_require={
+        'dev': DEV_REQUIREMENTS,
+    },
+    entry_points={
+        'console_scripts': [
+            'pyfamilysafety=pyfamilysafety:main',
+        ]
+    },
+    python_requires='>=3.8, <4',
+)
