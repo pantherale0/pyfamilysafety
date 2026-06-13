2e044db3a5df2389909c521f8333a05210a5003e
Jordan Harvey <jordan@hrvy.uk>
2026-06-13 14:46:54 +0100

    Refactor account update process to improve screentime usage handling
    - Consolidated multiple API calls in the Account class's update method into a single asyncio.gather call for efficiency.
    - Introduced new methods for applying screentime and application usage data.
    - Added a method to define the default time range for usage queries.
    - Streamlined the retrieval of devices, overrides, and applications to enhance code clarity and maintainability.

--- pyfamilysafety/account.py
+++ pyfamilysafety/account.py
@@ -54,14 +54,71 @@ class Account:
 
     async def update(self) -> None:
         """Update all account details."""
-        await self.get_screentime_usage()
-        coros = [self._get_devices(), self._get_overrides(), self._get_applications(), self._get_account_balance()]
-        await asyncio.gather(*coros)
+        begin_time, end_time = self._default_usage_time_range()
+        (
+            devices_response,
+            device_usage,
+            application_usage,
+            overrides_response,
+            _,
+        ) = await asyncio.gather(
+            self._api.async_get_user_devices(user_id=self.user_id),
+            self._api.async_get_user_device_screentime_usage(
+                user_id=self.user_id,
+                begin_time=begin_time,
+                end_time=end_time,
+                device_count=4,
+                platform="ALL",
+            ),
+            self._api.async_get_user_app_screentime_usage(
+                user_id=self.user_id,
+                begin_time=begin_time,
+                end_time=end_time,
+                platform="ALL",
+            ),
+            self._api.async_get_override_device_restrictions(user_id=self.user_id),
+            self._get_account_balance(),
+        )
+        self._apply_screentime_usage(device_usage.get("json"), application_usage.get("json"))
+        self.devices = Device.from_dict(devices_response.get("json"), self.screentime_usage)
+        self._apply_applications()
+        self._update_device_blocked(overrides_response.get("json"))
         for cb in self._account_callbacks:
             if is_awaitable(cb):
                 await cb()
             else:
                 cb()
+
+    def _default_usage_time_range(self) -> tuple[str, str]:
+        """Returns the default begin/end time query params for today's usage."""
+        start_time = localise_datetime(datetime.combine(date.today(), time(0, 0, 0), tzinfo=API_TIMEZONE))
+        end_time = localise_datetime(datetime.combine(date.today(), time(23, 59, 59), tzinfo=API_TIMEZONE))
+        return (
+            quote_plus(start_time.strftime('%Y-%m-%dT%H:%M:%S%z')),
+            quote_plus(end_time.strftime('%Y-%m-%dT%H:%M:%S%z')),
+        )
+
+    def _apply_screentime_usage(self, device_usage: dict, application_usage: dict) -> None:
+        """Store screentime usage payloads on the account."""
+        self.screentime_usage = device_usage
+        self.today_screentime_usage = device_usage["deviceUsageAggregates"]["totalScreenTime"]
+        self.average_screentime_usage = device_usage["deviceUsageAggregates"]["dailyAverage"]
+        self.application_usage = application_usage
+
+    def _apply_applications(self) -> list[Application]:
+        """Refresh application state from the latest activity report."""
+        if self.application_usage is None:
+            raise ValueError("Application usage not collected, call 'get_screentime_usage' first.")
+        parsed_applications = Application.from_app_activity_report(
+            self.application_usage,
+            self._api,
+            self.user_id)
+        for app in parsed_applications:
+            try:
+                self.get_application(app.app_id).update(app)
+            except IndexError:
+                self.applications.append(app)
+        return self.applications
 
     async def _get_devices(self) -> list[Device]:
         """Returns all devices on the account."""
@@ -77,18 +134,7 @@ class Account:
 
     async def _get_applications(self) -> list[Application]:
         """Returns all applications on the account."""
-        if self.application_usage is None:
-            raise ValueError("Application usage not collected, call 'get_screentime_usage' first.")
-        parsed_applications = Application.from_app_activity_report(
-            self.application_usage,
-            self._api,
-            self.user_id)
-        for app in parsed_applications:
-            try:
-                self.get_application(app.app_id).update(app)
-            except IndexError:
-                self.applications.append(app)
-        return self.applications
+        return self._apply_applications()
 
     async def _get_account_balance(self):
         """Updates the account balance."""
@@ -115,33 +161,32 @@ class Account:
             default = True
             end_time = localise_datetime(datetime.combine(date.today(), time(23,59,59), tzinfo=API_TIMEZONE))
 
-        device_usage = await self._api.async_get_user_device_screentime_usage(
+        begin_time = quote_plus(start_time.strftime('%Y-%m-%dT%H:%M:%S%z'))
+        end_time_param = quote_plus(end_time.strftime('%Y-%m-%dT%H:%M:%S%z'))
+
+        device_usage, application_usage = await asyncio.gather(
+            self._api.async_get_user_device_screentime_usage(
                 user_id=self.user_id,
-                begin_time=quote_plus(start_time.strftime('%Y-%m-%dT%H:%M:%S%z')),
-                end_time=quote_plus(end_time.strftime('%Y-%m-%dT%H:%M:%S%z')),
+                begin_time=begin_time,
+                end_time=end_time_param,
                 device_count=device_count,
                 platform=platform
-            )
-
-        application_usage = await self._api.async_get_user_app_screentime_usage(
+            ),
+            self._api.async_get_user_app_screentime_usage(
                 user_id=self.user_id,
-                begin_time=quote_plus(start_time.strftime('%Y-%m-%dT%H:%M:%S%z')),
-                end_time=quote_plus(end_time.strftime('%Y-%m-%dT%H:%M:%S%z')),
+                begin_time=begin_time,
+                end_time=end_time_param,
                 platform=platform
-            )
+            ),
+        )
 
         if default:
-            self.screentime_usage = device_usage.get("json")
-            self.today_screentime_usage = self.screentime_usage["deviceUsageAggregates"]["totalScreenTime"]
-            self.average_screentime_usage = self.screentime_usage["deviceUsageAggregates"]["dailyAverage"]
-            self.application_usage = application_usage.get("json")
+            self._apply_screentime_usage(device_usage.get("json"), application_usage.get("json"))
             return self.screentime_usage
-        else:
-            # don't actually set a value
-            return {
-                "devices": device_usage.get("json"),
-                "applications": application_usage.get("json")
-            }
+        return {
+            "devices": device_usage.get("json"),
+            "applications": application_usage.get("json")
+        }
 
     def get_device(self, device_id) -> Device:
         """Returns a single device."""
@@ -221,19 +266,20 @@ class Account:
     @classmethod
     async def from_dict(cls, api: FamilySafetyAPI, raw_response: dict, experimental: bool) -> list['Account']:
         """Converts a roster request response to an array."""
-        response = []
+        accounts = []
         if "members" in raw_response.keys():
             members = raw_response.get("members")
             for member in members:
                 if member.get("isDigitalSafetyEnabled"):
-                    self = cls(api)
-                    self.user_id = member.get("id")
-                    self.role = member.get("role")
-                    self.profile_picture = member.get("profilePicUrl")
-                    self.first_name = member.get("user").get("firstName")
-                    self.surname = member.get("user").get("lastName")
-                    self.experimental = experimental
-                    await self.update()
-                    response.append(self)
+                    account = cls(api)
+                    account.user_id = member.get("id")
+                    account.role = member.get("role")
+                    account.profile_picture = member.get("profilePicUrl")
+                    account.first_name = member.get("user").get("firstName")
+                    account.surname = member.get("user").get("lastName")
+                    account.experimental = experimental
+                    accounts.append(account)
+            if accounts:
+                await asyncio.gather(*(account.update() for account in accounts))
 
-        return response
+        return accounts
